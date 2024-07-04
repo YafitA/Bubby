e0fa0d2d8ecdb31dcba576307c607ee5c88801d3
shirp <shir.p999@gmail.com>
2024-07-04 15:46:32 +0300

    backend - API functions

--- Bubby/src/backend/models.py
+++ Bubby/src/backend/models.py
@@ -22,16 +22,29 @@ class User(BaseModel):
     challenges_done: List[Challenge] = Field(...)
     Score: int = Field(...)
 
+class UserUpdate(BaseModel):
+    name: Optional[str]
+    families: Optional[List[str]]
+    challenges_done: Optional[List[Challenge]]
+    Score: Optional[int]
+
 
 class Family(BaseModel):
     id: str = Field(default_factory=uuid.uuid4, alias="_id")
     last_name: str = Field(...)
     users: List[User] = Field(...)
 
+class FamilyUpdate(BaseModel):
+    last_name: Optional[str]
+    users: Optional[List[User]]
+
 
 class Photo_Album(BaseModel):
     id: str = Field(default_factory=uuid.uuid4, alias="_id")
     images: List[Image] = Field(...)
+
+class Photo_AlbumUpdate(BaseModel):
+    images: Optional[List[Image]]
 
 
 
